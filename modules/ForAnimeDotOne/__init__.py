b8511edefb0544de23afb063f6d95d74fd2103f7
Majroch <jakuboch4@gmail.com>
2020-08-10 12:27:57 +0200

    small update for module

--- modules/ForAnimeDotOne/__init__.py
+++ modules/ForAnimeDotOne/__init__.py
@@ -6,11 +6,9 @@ from modules.Logger import Logger
 class Site(_Site):
     def checkForChanges(self, url: str) -> bool:
         bs4 = self.getSite(url)
-        episodes = bs4.findAll("ul", {"class": "episodios"})
-        if len(episodes) < 1:
+        episodes = bs4.find("ul", {"class": "episodios"})
+        if not episodes:
             return False
-        else:
-            episodes = episodes[0]
         last_ep = episodes.findAll("a")
         last_ep = last_ep[len(last_ep)-1]['href']
         last_ep_config = self.getLastEp(url)
